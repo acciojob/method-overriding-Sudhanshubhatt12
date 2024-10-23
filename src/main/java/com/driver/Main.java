8e01a66e1ea1b5fecb3705336cb03c3ea2083cb7
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2024-10-23 23:27:37 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class Main {
+
 
 public class A {
     public String meth() {
@@ -16,7 +16,7 @@ public class B extends A {
     }
 }
 
-
+public class Main {
     public static void main(String[] args) {
        
         B obj = new B();
